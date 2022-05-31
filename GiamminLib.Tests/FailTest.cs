65b5ab1bf8166f28c7fc8bf3b8066faa150bd8ca
giammin <giammin@gmail.com>
2022-05-31 13:08:03 +0200

    check test fail

---
+++ GiamminLib.Tests/FailTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace GiamminLib.Tests
+{
+    [TestFixture]
+    public class FailTest
+    {
+
+        [Test]
+        public void Fail(string clearString)
+        {
+            Assert.Fail( "this test is meant to fail");
+        }
+    }
+}
